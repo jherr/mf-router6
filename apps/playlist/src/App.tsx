2c0fcce0eb0aefdb83db7a6f3b868cdc9c4be4d6
Jack Herrington <jherr@pobox.com>
2022-05-22 08:44:07 -0700

    Safe runtime sharing

--- apps/playlist/src/App.tsx
+++ apps/playlist/src/App.tsx
@@ -1,9 +1,38 @@
 import React from "react";
 import { AppShell } from "ui";
 
+import { MoviesContent } from "movies-content";
 import { Playlist } from "playlist-content";
 // @ts-ignore
-import MoviesContent from "movies/Movies";
+const MoviesContentRuntime = React.lazy(() => import("movies/Movies"));
+
+class ErrorBoundary extends React.Component<
+  {
+    children: React.ReactNode;
+  },
+  {
+    hasError: boolean;
+  }
+> {
+  constructor(props: { children: React.ReactNode }) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch() {}
+
+  render() {
+    if (this.state.hasError) {
+      return <MoviesContent />;
+    }
+
+    return this.props.children;
+  }
+}
 
 function App() {
   return (
@@ -13,7 +42,11 @@ function App() {
       routes={[
         {
           path: "/",
-          element: MoviesContent,
+          element: () => (
+            <ErrorBoundary>
+              <MoviesContentRuntime />
+            </ErrorBoundary>
+          ),
         },
         {
           path: "/playlist",
